fabfddf6ccada9fe67ad32beca21e8e7444f089c
osiyo <merewall@gmail.com>
2021-08-29 15:40:03 -0400

    adding offline functionality with caching in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,90 @@
 console.log("Hi from your service-worker.js file!");
+
+// STATIC CACHE
+const CACHE_NAME = "static-cache-v2";
+// DATA CACHE
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/assets/css/styles.css',
+  '/assets/js/index.js',
+  '/assets/js/db.js',
+  '/assets/images/icons/icon-192x192.png',
+  '/assets/images/icons/icon-512x512.png',
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+];
+
+// install
+self.addEventListener("install", function (evt) {
+    // pre cache image data
+    // evt.waitUntil(
+    //   caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+    //   );
+      
+    // pre cache all static assets
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
+  });
+
+  // activate service worker and remove old cache data
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+
+    self.clients.claim();
+  });
+
+  // Enable service worker to intercept network requests
+  self.addEventListener('fetch', function(evt) {
+    
+    // cache responses for requests for data
+    if (evt.request.url.includes('/api/')) {
+        console.log('[Service Worker] Fetch (data)', evt.request.url);
+
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(evt.request);
+                    });
+            })
+        );
+        
+        return;
+    }
+    
+    // serve static files from the cache - allows page to be accessible offline
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(evt.request).then(response => {
+            return response || fetch(evt.request);
+          });
+        })
+      );
+    });
